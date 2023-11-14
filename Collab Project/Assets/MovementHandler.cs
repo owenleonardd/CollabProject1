581c49ff0ba29a7f3234f519cba2a03fff922e9e
owenleonardd <100028058@mvla.net>
2023-11-14 09:25:23 -0800

    Only able to switch gravity on ground

--- Collab Project/Assets/MovementHandler.cs
+++ Collab Project/Assets/MovementHandler.cs
@@ -6,48 +6,39 @@ public class MovementHandler : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 10f;
-    public float switchCooldown = 1f;
-    private Vector2 _currVector = new Vector2(0, -1);
-    private bool _canSwitchGravity = true;
+    private Vector2 _currVector;
 
     private Rigidbody2D rb;
-    private float _timeSinceLastSwitch = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _currVector = new Vector2(0, -1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _timeSinceLastSwitch += Time.deltaTime;
-        if (_timeSinceLastSwitch >= switchCooldown || IsGrounded())
-        {
-            _canSwitchGravity = true;
-        }
         rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal"), 0).normalized * speed);
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce),ForceMode2D.Impulse);
         }
 
         rb.AddForce(new Vector2(-rb.velocity.x * 0.5f, 0));
         
-        if (Input.GetButtonDown("Fire1") && _canSwitchGravity)
+        if (Input.GetButtonDown("Fire1") && IsGrounded())
         {
-            switchGravity();
+            SwitchGravity();
         }
     }
 
-    private void switchGravity()
+    private void SwitchGravity()
     {
         rb.gravityScale*= -1;
         _currVector *= -1;
         jumpForce*=-1;
-        _canSwitchGravity = false;
-        _timeSinceLastSwitch = 0;
     }
     private bool IsGrounded()
     {
