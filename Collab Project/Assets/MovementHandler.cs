a9178fb1d66ced680d0f9cc6db266d8cb3c5e777
owenleonardd <100028058@mvla.net>
2023-11-14 08:46:55 -0800

    Created Player Prefab + Updated MovementHandler

--- Collab Project/Assets/MovementHandler.cs
+++ Collab Project/Assets/MovementHandler.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class MovementHandler : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-
+    public float speed = 5f;
     public float jumpForce = 10f;
+    public float switchCooldown = 1f;
+    private Vector2 _currVector = new Vector2(0, -1);
+    private bool _canSwitchGravity = true;
 
     private Rigidbody2D rb;
+    private float _timeSinceLastSwitch = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,25 +22,36 @@ public class MovementHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         float x = Input.GetAxisRaw("Horizontal");
-    
-         Vector2 moveDir = new Vector2(x, 0).normalized;
-    
-         rb.velocity = new Vector2(moveDir.x * moveSpeed, rb.velocity.y);
-    
-         if (Input.GetKeyDown(KeyCode.UpArrow))
-         {
-             if (IsGrounded())
-             {
-                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-             }
-         }
-         
+        _timeSinceLastSwitch += Time.deltaTime;
+        if (_timeSinceLastSwitch >= switchCooldown || IsGrounded())
+        {
+            _canSwitchGravity = true;
+        }
+        rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0));
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+        }
+
+        rb.AddForce(new Vector2(-rb.velocity.x * 0.5f, 0));
+        
+        if (Input.GetButtonDown("Fire1") && _canSwitchGravity)
+        {
+            switchGravity();
+        }
     }
 
+    private void switchGravity()
+    {
+        rb.gravityScale*= -1;
+        _currVector *= -1;
+        jumpForce*=-1;
+        _canSwitchGravity = false;
+        _timeSinceLastSwitch = 0;
+    }
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, LayerMask.GetMask("Ground"));
-        return raycastHit.collider != null;
+        return Physics2D.Raycast(transform.position, _currVector, 1.1f, LayerMask.GetMask("Ground"));
     }
+    
 }
