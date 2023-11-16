b4c86feb67447a28310d2e3b4864a00bf4eb7215
owenleonardd <100028058@mvla.net>
2023-11-16 09:44:22 -0800

    Imported WebGL Builder

--- Collab Project/Assets/CollisionHandler.cs
+++ Collab Project/Assets/CollisionHandler.cs
@@ -7,13 +7,32 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int minY;
+
+    private void Start()
+    {
+        minY = Math.Abs(minY);
+    }
+
+    private void Update()
+    {
+        if (Math.Abs(transform.position.y) > minY)
+        {
+            Die();
+        }
+    }
 
     public void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("OBSTACLE");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
