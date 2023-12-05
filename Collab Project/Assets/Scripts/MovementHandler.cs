cedafc4c75accae868959d14cbf5a346433abf93
owenleonardd <100028058@mvla.net>
2023-12-05 08:35:37 -0800

    Slime Trail Fixed From Merge Conflict

--- Collab Project/Assets/Scripts/MovementHandler.cs
+++ Collab Project/Assets/Scripts/MovementHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
 public class MovementHandler : MonoBehaviour
@@ -9,15 +10,21 @@ public class MovementHandler : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 10f;
     
+    public Tilemap slimeTrail;
+    public Tile slimeTile;
+
+    private Tile _slimeTileRotated;
     private Rigidbody2D _rigidbody2D;
     private bool _isGrounded;
-
-    private Animator anim;
+    
+    private bool _slimeTrailActive;
+    public float slimeMultiplier;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        _slimeTrailActive = false;
+
     }
     
     private void Update()
@@ -25,11 +32,6 @@ public class MovementHandler : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
             _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            anim.SetBool("jumping", true);
-        }
-        else
-        {
-            anim.SetBool("jumping", false);
         }
         
         if (Input.GetButtonDown("Fire1"))
@@ -37,12 +39,25 @@ public class MovementHandler : MonoBehaviour
             SwitchGravity();
             
         }
+        
+        if (Input.GetButtonDown("Fire2"))
+        {
+            _slimeTrailActive = !_slimeTrailActive;
+        }
     }
     
     private void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         _rigidbody2D.velocity = new Vector2(horizontalInput * speed, _rigidbody2D.velocity.y);
+        if (_slimeTrailActive && _isGrounded)
+        {
+            //need to offset the tile row by 1 to get the correct tile
+            if(jumpForce > 0)
+                slimeTrail.SetTile(slimeTrail.WorldToCell(transform.position + new Vector3(0f, -1f, 0f)), slimeTile);
+            else
+                slimeTrail.SetTile(slimeTrail.WorldToCell(transform.position + new Vector3(0f, 1f, 0f)), slimeTile);
+        }
     }
     
     private void OnCollisionEnter2D(Collision2D other)
@@ -52,7 +67,24 @@ public class MovementHandler : MonoBehaviour
             _isGrounded = true;
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("SlimeTrail"))
+        {
+            speed *= slimeMultiplier;
+        }
+    }
     
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("SlimeTrail"))
+        {
+            speed /= slimeMultiplier;
+        }
+    }
+
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
@@ -64,6 +96,7 @@ public class MovementHandler : MonoBehaviour
     private void SwitchGravity()
     {
         _rigidbody2D.gravityScale *= -1;
+        // speed *= -1;
         jumpForce *= -1;
         StartCoroutine(Rotate());
     }
@@ -78,5 +111,7 @@ public class MovementHandler : MonoBehaviour
             yield return null;
         }
     }
+    
+    
     
 }
