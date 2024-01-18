48bb218601dde50b449ef25bac67844fdc3e9dae
owenleonardd <100028058@mvla.net>
2024-01-18 09:31:36 -0800

    Fade Portal

--- Collab Project/Assets/Scripts/NextLevel.cs
+++ Collab Project/Assets/Scripts/NextLevel.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour
 {
     
     private bool playerInPortal = false;
+    public Image fadeImage;
+    
+    private FadeInOut fadeScript;
+    
+    private void Start()
+    {
+        fadeScript = fadeImage.GetComponent<FadeInOut>();
+    }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -23,21 +32,21 @@ public class NextLevel : MonoBehaviour
             playerInPortal = false;
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && playerInPortal)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   
+            StartCoroutine(FadeToScene());
         }
         
     }
-    
-    public IEnumerator
+
+    public IEnumerator FadeToScene()
+    {
+        fadeScript.FadeIn();
+        yield return new WaitForSeconds(1/fadeScript.fadeSpeed);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
