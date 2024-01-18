4a9f87ee6a76c6f997fb02f09d828891fbcd1766
Lyric Manson <100027205@mvla.net>
2024-01-18 09:05:17 -0800

    cleaned up portal animator

--- Collab Project/Assets/Scripts/PortalAnimationController.cs
+++ Collab Project/Assets/Scripts/PortalAnimationController.cs
@@ -7,21 +7,16 @@ public class PortalAnimationController : MonoBehaviour
 {
     private Animator PortalAnimator;
 
-    private bool playerInPortal;
     // Start is called before the first frame update
     void Start()
     {
         PortalAnimator = GetComponent<Animator>();
-        playerInPortal = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && playerInPortal)
-        {
-            Debug.Log("Player in portal and e key pressed");
-        }
+      
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -29,7 +24,6 @@ public class PortalAnimationController : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             PortalAnimator.SetBool("playerOnPortal", true);
-            playerInPortal = true;
 
         }
         //throw new NotImplementedException();
@@ -40,7 +34,6 @@ public class PortalAnimationController : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             PortalAnimator.SetBool("playerOnPortal", false);
-            playerInPortal = false;
         }
         //throw new NotImplementedException();
     }
